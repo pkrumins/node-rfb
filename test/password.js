54bd7a83b02f5399566866cea8119e49401e082f
James Halliday <mail@substack.net>
2011-04-21 07:23:56 -0700

    a passing test for passwords finally

--- test/password.js
+++ test/password.js
@@ -1,11 +1,47 @@
 var assert = require('assert');
 var qemu = require('./lib/qemu');
+var RFB = require('rfb');
 
-var q = qemu('-vnc', ':0,password', qemu.img, '-monitor', 'stdio');
-
-q.stdin.write('change vnc password\n');
-q.stdin.write('moo\n');
-
-q.stdout.once('data', function (buf) {
-    console.dir(buf.toString());
-});
+exports.password = function () {
+    //var port = Math.floor(Math.random() * (Math.pow(2,16) - 10000)) + 10000;
+    var port = 5900;
+    
+    var q = qemu(
+        '-vnc', ':' + (port - 5900) + ',password',
+        qemu.img, '-monitor', 'stdio'
+    );
+    
+    q.stdout.on('data', function fn (buf) {
+        if (buf.toString().match(/^\(qemu\)/m)) {
+            clearTimeout(to.monitor);
+            
+            q.stdout.removeListener('data', fn);
+            q.stdin.write('change vnc password\n');
+            q.stdin.write('moo\n');
+            
+            setTimeout(function () {
+                var r = new RFB({
+                    port : port,
+                    password : 'moo',
+                    securityType : 'vnc',
+                });
+                r.requestRedraw();
+                r.dimensions(function (dims) {
+                    clearTimeout(to.dimensions);
+                    assert.eql(dims, { width : 720, height : 400 });
+                    q.stdin.write('quit\n');
+                });
+            }, 5000);
+        }
+    });
+    
+    var to = 'monitor dimensions'
+        .split(' ')
+        .reduce(function (acc, name) {
+            acc[name] = setTimeout(function () {
+                assert.fail('never reached ' + name);
+            }, 10000);
+            return acc;
+        }, {})
+    ;
+};
